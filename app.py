db1e2913312718972e0a8c09cb26613b4f2b7e9a
kubixananas <kubasaj210@gmail.com>
2026-09-21 16:24:27 +0200

    feat: Pobieranie podstawowych danych

---
+++ app.py
@@ -0,0 +1,8 @@
+dzienUrodzenia = int(input("Wprowadź dzień urodzenia: "))
+miesiacUrodzenia = int(input("Wprowadź miesiąc urodzenia: "))
+
+A = 100 + dzienUrodzenia
+B = 200 + miesiacUrodzenia
+C = 160 + dzienUrodzenia + miesiacUrodzenia
+D = 222 - dzienUrodzenia
+
